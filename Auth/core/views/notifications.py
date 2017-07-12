b88ceacb532565c62301d40dcc85e8ac54f70b4d
Kaleb Porter <porowns@gmail.com>
2017-07-12 11:14:04 -0400

    Refactored notifications & profiles to use login check decorator.

--- Auth/core/views/notifications.py
+++ Auth/core/views/notifications.py
@@ -5,50 +5,38 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from core.models import Profile, Notification, Game, Event
 from . import base
+from core.decorators import login_required
+
 ## NOTIFICATIONS
+@login_required
 def all_notifications(request, username):
-    if request.user.is_authenticated():
-        user = request.user
-        user_profile = Profile.objects.get(user=user)
-        notifications = Notification.objects.filter(user=user)
-        return render(request, 'models/notifications/all_notitifications.html', context={})
-    else:
-        return redirect('login')
+    user = request.user
+    user_profile = Profile.objects.get(user=user)
+    notifications = Notification.objects.filter(user=user)
+    return render(request, 'models/notifications/all_notitifications.html', context={})
 
 def view_notification(request, pk):
-    if request.user.is_authenticated():
-        user = request.user
-        user_profile = Profile.objects.get(user=user)
-        notifications = Notification.objects.filter(user=user)
-        return render(request, 'models/notifications/view_notification.html', context={})
-    else:
-        return redirect('login')
+    user = request.user
+    user_profile = Profile.objects.get(user=user)
+    notifications = Notification.objects.filter(user=user)
+    return render(request, 'models/notifications/view_notification.html', context={})
 
 def create_notification(request):
-    if request.user.is_authenticated():
-        user = request.user
-        user_profile = Profile.objects.get(user=user)
-        notifications = Notification.objects.filter(user=user)
-        return render(request, 'models/notifications/create_notification.html', context={})
-    else:
-        return redirect('login')
+    user = request.user
+    user_profile = Profile.objects.get(user=user)
+    notifications = Notification.objects.filter(user=user)
+    return render(request, 'models/notifications/create_notification.html', context={})
 
 def delete_notification(request, pk):
-    if request.user.is_authenticated():
-        user = request.user
-        user_profile = Profile.objects.get(user=user)
-        notifications = Notification.objects.filter(user=user)
-        return redirect('dashboard')
-    else:
-        return redirect('login')
+    user = request.user
+    user_profile = Profile.objects.get(user=user)
+    notifications = Notification.objects.filter(user=user)
+    return redirect('dashboard')
 
 def modify_notification(request, pk):
-    if request.user.is_authenticated():
-        user = request.user
-        user_profile = Profile.objects.get(user=user)
-        notifications = Notification.objects.filter(user=user)
-        return render(request, 'models/notifications/modify_notification.html', context={})
-    else:
-        return redirect('login')
+    user = request.user
+    user_profile = Profile.objects.get(user=user)
+    notifications = Notification.objects.filter(user=user)
+    return render(request, 'models/notifications/modify_notification.html', context={})
 
 
--- Auth/core/views/profile.py
+++ Auth/core/views/profile.py
@@ -5,164 +5,156 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from core.models import Profile, Notification, Game, Event
 from . import base
+from core.decorators import login_required
+
 ## PROFILES
+@login_required
 def all_profiles(request):
-    if request.user.is_authenticated():
-        user = request.user
-        profile = Profile.objects.get(user=user)
-        notifications = Notification.objects.filter(user=user)
-        if user.groups.filter(name__in=['Officer', 'Leadership', 'Admin']).exists():
-            return render(
-                    request,
-                    'profiles/all_profiles.html',
-                    context={
-                        'profiles': Profile.objects.all(),
-                        'profile': profile,
-                        'notifications': notifications,
-                        }
-                    )
-        else:
-            return redirect('no_permissions')
-    else:
-        return redirect('login')
-
-def view_profile(request, pk):
-    if request.user.is_authenticated():
-        user = request.user
-        user_profile = Profile.objects.get(user=user)
-        notifications = Notification.objects.filter(user=user)
+    user = request.user
+    profile = Profile.objects.get(user=user)
+    notifications = Notification.objects.filter(user=user)
+    if user.groups.filter(name__in=['Officer', 'Leadership', 'Admin']).exists():
         return render(
                 request,
-                'profiles/view_profile.html',
+                'profiles/all_profiles.html',
                 context={
-                    'user_profile': user_profile,
+                    'profiles': Profile.objects.all(),
+                    'profile': profile,
                     'notifications': notifications,
-                    'profile': Profile.objects.get(pk=pk),
                     }
                 )
     else:
-        return redirect('login')
+        return redirect('no_permissions')
 
+@login_required
+def view_profile(request, pk):
+    user = request.user
+    user_profile = Profile.objects.get(user=user)
+    notifications = Notification.objects.filter(user=user)
+    return render(
+            request,
+            'profiles/view_profile.html',
+            context={
+                'user_profile': user_profile,
+                'notifications': notifications,
+                'profile': Profile.objects.get(pk=pk),
+                }
+            )
+
+@login_required
 def create_profile(request):
-    if request.user.is_authenticated():
-        user = request.user
+    user = request.user
 
-        if request.method == 'POST':
-            biography = request.POST.get('biography')
-            game = request.POST.get('games')
-            twitter = request.POST.get('twitter')
-            steam = request.POST.get('steam')
-            blizzard = request.POST.get('blizzard')
+    if request.method == 'POST':
+        biography = request.POST.get('biography')
+        game = request.POST.get('games')
+        twitter = request.POST.get('twitter')
+        steam = request.POST.get('steam')
+        blizzard = request.POST.get('blizzard')
 
-            profile = Profile(user=user)
-            profile.save()
-            if biography:
-                profile.biography = biography
-            if game:
-                profile.games.add(game)
-            if twitter:
-                profile.twitter = twitter
-            if steam:
-                profile.steam = steam
-            if blizzard:
-                profile.blizzard = blizzard
+        profile = Profile(user=user)
+        profile.save()
+        if biography:
+            profile.biography = biography
+        if game:
+            profile.games.add(game)
+        if twitter:
+            profile.twitter = twitter
+        if steam:
+            profile.steam = steam
+        if blizzard:
+            profile.blizzard = blizzard
 
-            profile.save()
+        profile.save()
 
-            return redirect('dashboard')
-        else:
-            form = ProfileForm()
-        return render(
-                request,
-                'profiles/create_profile.html',
-                context={
-                    'form': form,
-                    }
-                )
+        return redirect('dashboard')
     else:
-        return redirect('login')
+        form = ProfileForm()
+    return render(
+            request,
+            'profiles/create_profile.html',
+            context={
+                'form': form,
+                }
+            )
 
+@login_required
 def delete_profile(request, pk):
-    if request.user.is_authenticated():
-        user = request.user
-        user_profile = Profile.objects.get(user=user)
-        notifications = Notification.objects.filter(user=user)
+    user = request.user
+    user_profile = Profile.objects.get(user=user)
+    notifications = Notification.objects.filter(user=user)
 
-        if user_profile == Profile.objects.get(pk=pk):
-            user_profile.delete()
-            return redirect('dashboard')
-        else:
-            return redirect('no_permissions')
+    if user_profile == Profile.objects.get(pk=pk):
+        user_profile.delete()
+        return redirect('dashboard')
     else:
-        return redirect('login')
+        return redirect('no_permissions')
 
+@login_required
 def modify_profile(request, pk):
-    if request.user.is_authenticated():
-        user = request.user
-        user_profile = Profile.objects.get(user=user)
-        notifications = Notification.objects.filter(user=user)
+    user = request.user
+    user_profile = Profile.objects.get(user=user)
+    notifications = Notification.objects.filter(user=user)
 
-        # Build list of currently added
-        game_list = []
-        for game in user_profile.games.all():
-            game_list.append(game.title)
-        games = Game.objects.exclude(title__in=game_list)
+    # Build list of currently added
+    game_list = []
+    for game in user_profile.games.all():
+        game_list.append(game.title)
+    games = Game.objects.exclude(title__in=game_list)
 
-        if request.method == 'POST':
-            form = ProfileForm(request.POST)
-            if form.is_valid():
-                user_profile.biography = request.POST.get('biography')
-                user_profile.twitter = request.POST.get('twitter')
-                user_profile.steam = request.POST.get('steam')
-                user_profile.blizzard = request.POST.get('blizzard')
-                user_profile.save()
-                return redirect('view-profile', pk=user_profile.id)
-        else:
-            form = ProfileForm()
-
-        return render(
-                request,
-                'profiles/modify_profile.html',
-                context={
-                    'form': form,
-                    'biography': user_profile.biography,
-                    'twitter': user_profile.twitter,
-                    'steam': user_profile.steam,
-                    'blizzard': user_profile.blizzard,
-                    'games': Game.objects.all(),
-                    'profile': user_profile,
-                    'user': user
-                    }
-                )
+    if request.method == 'POST':
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+            user_profile.biography = request.POST.get('biography')
+            user_profile.twitter = request.POST.get('twitter')
+            user_profile.steam = request.POST.get('steam')
+            user_profile.blizzard = request.POST.get('blizzard')
+            user_profile.save()
+            return redirect('view-profile', pk=user_profile.id)
     else:
-        return redirect('login')
+        form = ProfileForm()
 
+    return render(
+            request,
+            'profiles/modify_profile.html',
+            context={
+                'form': form,
+                'biography': user_profile.biography,
+                'twitter': user_profile.twitter,
+                'steam': user_profile.steam,
+                'blizzard': user_profile.blizzard,
+                'games': Game.objects.all(),
+                'profile': user_profile,
+                'user': user
+                }
+            )
+
+@login_required
 def profile_add_game(request, pk, game):
-    if request.user.is_authenticated():
-        user = request.user
-        passed_profile = Profile.objects.get(pk=pk)
-        user_profile = Profile.objects.get(user=user)
-        if passed_profile.id != user_profile.id:
-            print(passed_profile.id)
-            print(user_profile.id)
-            return redirect('no_permissions')
+    user = request.user
+    passed_profile = Profile.objects.get(pk=pk)
+    user_profile = Profile.objects.get(user=user)
+    if passed_profile.id != user_profile.id:
+        print(passed_profile.id)
+        print(user_profile.id)
+        return redirect('no_permissions')
 
-        profile = Profile.objects.get(pk=pk)
-        profile.games.add(Game.objects.get(id=game))
-        profile.save()
-        return redirect('modify-profile', pk=pk)
+    profile = Profile.objects.get(pk=pk)
+    profile.games.add(Game.objects.get(id=game))
+    profile.save()
+    return redirect('modify-profile', pk=pk)
 
+@login_required
 def profile_remove_game(request, pk, game):
-     if request.user.is_authenticated():
-        user = request.user
-        passed_profile = Profile.objects.get(pk=pk)
-        user_profile = Profile.objects.get(user=user)
-        if passed_profile.id != user_profile.id:
-            print(passed_profile.id)
-            print(user_profile.id)
-            return redirect('no_permissions')
+    user = request.user
+    passed_profile = Profile.objects.get(pk=pk)
+    user_profile = Profile.objects.get(user=user)
+    if passed_profile.id != user_profile.id:
+        print(passed_profile.id)
+        print(user_profile.id)
+        return redirect('no_permissions')
 
-        profile = Profile.objects.get(pk=pk)
-        profile.games.remove(Game.objects.get(id=game))
-        profile.save()
-        return redirect('modify-profile', pk=pk)
+    profile = Profile.objects.get(pk=pk)
+    profile.games.remove(Game.objects.get(id=game))
+    profile.save()
+    return redirect('modify-profile', pk=pk)
