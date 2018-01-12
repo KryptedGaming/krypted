497fab780afd466151ba22d66dc4bf82b7f31327
porowns <porowns@gmail.com>
2018-01-11 20:20:37 -0500

    Add/remove Discord roles. Manage a users Discord roles.

--- app/modules/discord/models.py
+++ app/modules/discord/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 class DiscordToken(models.Model):
     access_token = models.CharField(max_length=255)
@@ -8,3 +8,7 @@ class DiscordToken(models.Model):
     username = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
+class DiscordRole(models.Model):
+    role_id = models.CharField(max_length=255, primary_key=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, blank=True)
--- app/modules/discord/utils.py
+++ app/modules/discord/utils.py
@@ -1,22 +1,68 @@
 from django.conf import settings
+from modules.discord.models import DiscordRole, DiscordToken
 import requests
+import json
 
 def viewDiscordGroups():
     url = settings.DISCORD_API_ENDPOINT + "/guilds/" + settings.DISCORD_SERVER_ID + "/roles"
     view_groups = requests.get(url, headers={'Authorization': 'Bot ' + settings.DISCORD_BOT_TOKEN}).json()
     print(view_groups)
     print(url)
-    print(headers)
 
-def createDiscordGroup():
+def addDiscordGroup(group):
+    """
+    Expects a string role_name and a Group object.
+    Creates a Discord Group in the auth database, as well as the Discord server.
+    """
     url = settings.DISCORD_API_ENDPOINT + "/guilds/" + settings.DISCORD_SERVER_ID + "/roles"
-    data={'name': 'test'}
-    headers={'Content-Type': 'application/x-www-form-urlencoded','Authorization': 'Bot ' + settings.DISCORD_BOT_TOKEN}
+    # Set channel name
+    data=json.dumps({'name': group.name})
     create_group = requests.post(url,
-        data={'name': 'test'},
+        data=data,
         headers={
             'Content-Type': 'application/x-www-form-urlencoded',
             'Authorization': 'Bot ' + settings.DISCORD_BOT_TOKEN
         }
     ).json()
-    print(create_group)
+    role = DiscordRole(role_id=create_group['id'], group=group)
+    role.save()
+    return role
+
+def removeDiscordGroup(role):
+    """
+    Expects a DiscordRole object.
+    Deletes the Discord Role from our database and the Discord server.
+    """
+    url = settings.DISCORD_API_ENDPOINT + "/guilds/" + settings.DISCORD_SERVER_ID + "/roles/" + str(role.role_id)
+    delete_group = requests.delete(url, headers={
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Bot ' + settings.DISCORD_BOT_TOKEN
+    })
+    role.delete()
+    print(delete_group)
+
+def addDiscordGroupToUser(user, role):
+    """
+    Expects a User object and DiscordRole object.
+    Adds the specified role to a user.
+    """
+    discord_id = DiscordToken.objects.get(user=user).userid
+    url = settings.DISCORD_API_ENDPOINT + "/guilds/" + settings.DISCORD_SERVER_ID + "/members/" +  discord_id + "/roles/" + str(role.role_id)
+    add_group_to_user = requests.put(url, headers={
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Bot ' + settings.DISCORD_BOT_TOKEN
+    })
+    print(add_group_to_user)
+
+def removeDiscordGroupFromUser(user, role):
+    """
+    Expects a User object and DiscordRole object.
+    Remove the specified role from a user.
+    """
+    discord_id = DiscordToken.objects.get(user=user).userid
+    url = settings.DISCORD_API_ENDPOINT + "/guilds/" + settings.DISCORD_SERVER_ID + "/members/" +  discord_id + "/roles/" + str(role.role_id)
+    remove_group_to_user = requests.delete(url, headers={
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Bot ' + settings.DISCORD_BOT_TOKEN
+    })
+    print(remove_group_to_user)
