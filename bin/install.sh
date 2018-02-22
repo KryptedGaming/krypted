0b54afcb6feb45af8f51bd974c5da746ddfef5d0
porowns <porowns@gmail.com>
2018-02-22 16:59:58 +0100

    Updates from server migration. Had some issues with logging, fixed them. Fixed some old UI issues

--- bin/install.sh
+++ bin/install.sh
@@ -2,6 +2,3 @@ apt-get install python3 python3-pip mysql-server python-dev libmysqlclient-dev a
 apt-get install screen unzip git redis-server curl libssl-dev libbz2-dev libffi-dev
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
-mkdir /var/log/auth
-touch /var/log/auth/eveonline.log /var/log/auth/kryptedauth.log
-chmod 777 /var/log/auth/eveonline.log /var/log/auth/kryptedauth.log
