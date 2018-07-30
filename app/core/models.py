bcef36ce5e9decddb921fdc7ad463ea78ecb6efa
porowns <porowns@gmail.com>
2018-07-30 23:38:54 +0200

    #73 - Rebuild core modules.

--- app/core/models.py
+++ app/core/models.py
@@ -1,115 +1,171 @@
+from django.contrib.auth.models import User as DjangoUser
+from django.contrib.auth.models import Group as DjangoGroup
 from django.db import models
-from django.contrib.auth.models import User, Group
+from django.conf import settings
 
+"""
+CORE MODELS
+These are the core models of Krypted Authentication. They describe user interaction.
+"""
+class User(DjangoUser):
+    """
+    User model for Krypted Authentication.
+    Extended off of the Django base user, with additional fields and properties.
+    """
+    # BASIC FIELDS
+    avatar = models.URLField(max_length=255, blank=True, null=True)
+    biography = models.TextField(blank=True, null=True)
+    region = models.CharField(max_length=2, choices=settings.REGIONS)
 
-class Game(models.Model):
-    title = models.CharField(max_length=24, unique=True)
-    leadership = models.ManyToManyField(User)
-    def __str__(self):
-        return self.title
-    def is_guild(self):
-        if Guild.objects.filter(pk=self.pk).count() > 0:
-            return True
-        else:
-            return False
+    # REFERENCES
+    guilds = models.ManyToManyField("Guild", blank=True, null=True)
 
-class Guild(Game):
-    image = models.URLField(max_length=255, blank=True, null=True)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, blank=True)
-    slug = models.CharField(max_length=24, blank=True, null=True)
-
-    def __str__(self):
-        return self.title
-
-class Notification(models.Model):
-    title = models.CharField(max_length=24)
-    message = models.CharField(max_length=500)
-    read = models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_created = models.DateField(auto_now=True)
-
-    def __str__(self):
-        return self.title
-
-    def mark_read(self):
-        try:
-            read = True
-            return True
-        except:
-            pass
-    def notify_user(self, user):
-        try:
-            self.title = "System"
-            self.user = user
-            self.save()
-        except:
-            pass
-
-class Profile(models.Model):
-    timezone_choices = (
-        ("EU", "EU"),
-        ("US", "US"),
-        ("AU", "AU")
+class Group(DjangoGroup):
+    """
+    Group model for Krypted Authentication.
+    Extended off of the Django base group, with additional fields and propertiesself.
+    Some notes:
+        - null guild means community-wide group
+    """
+    group_types = (
+        ("PUBLIC", "Public"),
+        ("PROTECTED", "PROTECTED"),
+        ("PRIVATE", "PRIVATE")
     )
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    # Profile Information
-    biography = models.CharField(max_length=1500, blank=True, null=True)
-    timezone = models.CharField(choices=timezone_choices, max_length=2, blank=True, null=True)
-    active = models.BooleanField(default=True)
+    # BASIC INFORMATION
+    description = models.TextField()
+    type = models.CharField(max_length=12, choices=group_types)
 
-    # Community Information
-    points = models.IntegerField(default=0)
-    games = models.ManyToManyField('Game', related_name='games')
-    guilds = models.ManyToManyField('Guild', related_name='guilds')
-
-    # Social
-    twitter = models.URLField(max_length=32, blank=True, null=True)
-    steam = models.URLField(max_length=255, blank=True, null=True)
-    blizzard = models.CharField(max_length=32, blank=True, null=True)
-    discord = models.CharField(max_length=32, blank=True, null=True)
-
-    def __str__(self):
-        return self.user.username
+    # REFERENCES
+    guild = models.OneToOneField("Guild", on_delete=models.SET_NULL, blank=True, null=True)
+    managers = models.ManyToManyField("User")
 
 class Event(models.Model):
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_posted = models.DateField(auto_now=True)
-    date_occuring = models.DateTimeField(auto_now=False)
-    title = models.CharField(max_length=32)
-    description = models.CharField(max_length=128, blank=True, null=True)
-    notes = models.CharField(max_length=32, blank=True, null=True)
-    guild = models.OneToOneField("Guild", on_delete=models.SET_NULL, null=True)
-    value = models.DecimalField(max_digits=2, decimal_places=1)
+    """
+    Event model for Krypted Authentication
+    This model is used to create community events that can be tracked for participation
+    Some notes:
+        - null guild means community-wide event
+        - null user means needs to be assigned
+    """
 
+    # BASIC INFORMATION
+    name = models.CharField(max_length=32)
+    description = models.TextField()
+    date = models.DateField()
+    password = models.CharField(max_length=5)
+    value = models.DecimalField(max_digits=2)
+
+    # REFERENCES
+    user = models.ForeignKey("User", on_delete=models.SET_NULL, blank=True, null=True)
+    guild = models.ForeignKey("Guild", on_delete=models.SET_NULL, blank=True, null=True)
+
+class Guild(models.Model):
+    """
+    Guild model for Krypted Authentication. Guilds are used to describe a particular set of players.
+    """
+    # BASIC INFORMATION
+    name = models.CharField(max_length=32)
+    slug = models.CharField(max_length=8)
+    date_formed = models.DateField(auto_now=True)
+
+    # REFERENCES
+    group = models.OneToOneField("Group", on_delete=models.SET_NULL)
+
+"""
+FUNCTIONAL MODELS
+These are models used to achieve processes
+"""
 class GroupRequest(models.Model):
-    status_choices = (
-            ("Pending", "Pending"),
-            ("Accepted", "Accepted"),
-            ("Rejected", "Rejected"),
-            ("Vetoed", "Vetoed")
-            )
-    id = models.AutoField(primary_key=True)
-    status = models.CharField(max_length=32, choices=status_choices)
-    date_requested = models.DateField(auto_now=True)
-    date_updated = models.DateField(auto_now=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    processed_by = models.ForeignKey(User, related_name='processor', on_delete=models.CASCADE, blank=True, null=True)
-    group = models.ForeignKey("GroupEntity", on_delete=models.CASCADE)
+    """
+    Group Request model for Krypted Authentication
+    Used by users to create group requests, which require management approvement
+    """
+    response_action_fields = (
+        "PENDING", "Pending",
+        "ACCEPTED", "Accepted",
+        "REJECTED", "Rejected",
+        "RETRACTED", "Retracted"
+    )
+    # AUTHENTICATION
+    request_user = models.ForeignKey("User", on_delete=models.CASCADE)
+    request_group = models.OneToOneField("Group", on_delete=models.CASCADE)
+    request_date = models.DateField(auto_now=True)
+
+    # MANAGEMENT
+    response_user = models.ForeignKey("User", on_delete=models.SET_NULL)
+    response_action = models.CharField(max_length=32, choices=response_action_fields)
+    response_date = models.DateField(blank=True, null=True)
+
+class GuildApplicationTemplate(models.Model):
+    """
+    Guild Application Template for Krypted Authentication
+    Used to build Guild applications, which can be filled out by users and recorded as GuildApplication() objects
+    """
+    # REFERENCES
+    guild = models.OneToOneField("Guild", on_delete=models.CASCADE)
+    questions = models.ManyToManyField("GuildApplicationQuestion")
+
+
+class GuildApplicationQuestion(models.Model):
+    """
+    Guild Application Question for Krypted Authentication
+    Used to be filled out by users in GuildApplication() objects, recorded as GuildApplicationResponse() objects
+    """
+    question_type_fields = (
+        ("RESPONSE", "Response"),
+        ("MODAL", "Modal")
+    )
+    # BASIC INFORMATION
+    name = models.CharField(max_length=32)
+    help_text = models.TextField()
+    type = models.CharField(max_length=16, choices=question_type_fields)
+
+class GuildApplicationResponse(models.Model):
+    # BASIC INFORMATION
+    response = models.TextField()
+    # REFERENCES
+    question = models.ForeignKey("GuildApplicationQuestion", on_delete=models.CASCADE)
+    application = models.ForeignKey("GuildApplication", on_delete=models.CASCADE)
+
+class GuildApplication(models.Model):
+    application_status_fields = (
+        ("PENDING", "Pending"),
+        ("ACCEPTED", "Accepted"),
+        ("REJECTED", "Rejected")
+    )
+    # BASIC INFORMATION
+    status = models.CharField(max_length=16, choices=application_status_fields)
+
+    # USER
+    request_user = models.ForeignKey("User", on_delete=models.CASCADE)
+    request_date = models.DateField(auto_now=True)
+
+    # MANAGEMENT
+    response_user = models.ForeignKey("User", on_delete=models.CASCADE)
+    response_date = models.DateField(blank=True, null=True)
+
+"""
+ABSTRACT MODELS
+These are abstract models, extended by other modules of the application
+"""
+class ModuleUser(models.Model):
+    """
+    User for third-party modules like Discourse, Discord, and Slack
+    """
+    external_id = models.IntegerField(blank=True, null=True)
+    user = models.OneToOneField("User", on_delete=models.CASCADE)
 
     class Meta:
-        permissions = (
-                ('manage_group_requests', u'Can manage group requests.'),
-                ('audit_group_requests', u'Can view the group request audit log.')
-        )
+        abstract=True
 
-class GroupEntity(models.Model):
-    group = models.OneToOneField(Group, on_delete=models.CASCADE, primary_key=True)
-    guild = models.ForeignKey(Guild, on_delete=models.CASCADE, blank=True, null=True)
-    description = models.CharField(max_length=512, blank=True)
-    hidden = models.BooleanField(default=True, help_text="Hidden from the apply menu.")
-    public = models.BooleanField(default=False, help_text="Automatically join upon request.")
-    managers = models.ManyToManyField(User, blank=True, help_text="Users who can accept/decline requests.")
+class ModuleGroup(models.Model):
+    """
+    Group for third-party modules like Discourse, Discord, and Slack
+    """
+    external_id = models.IntegerField(blank=True, null=True)
+    group = models.OneToOneField("Group", on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.group.name
+    class Meta:
+        abstract=True
