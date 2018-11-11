840e4a10455a3a7ee4e8e654bf440af01c0dc153
porowns <porowns@gmail.com>
2018-11-11 20:03:54 +0100

    Group quality of life additions

--- app/core/views/groups.py
+++ app/core/views/groups.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from django.contrib import messages
 from core.models import User, Group, GroupRequest
 from core.decorators import login_required, permission_required, services_required, staff_required
+from app.conf import discord as discord_settings
+from modules.discord.tasks import send_discord_message
 import logging
 
 logger = logging.getLogger(__name__)
@@ -16,6 +18,8 @@ def group_apply(request, group):
         request.user.groups.add(group.group)
         request.user.save()
     group_request.save()
+    if not group_request.request_group.type == "PUBLIC":
+        notify_discord_channel(group_request, group_request.request_group.guild)
     return redirect('groups')
 
 @login_required
@@ -27,6 +31,7 @@ def group_add_user(request, group_id, user_id):
     user.groups.add(group_request.request_group)
     user.save()
     group_request.save()
+    notify_user(group_request, "APPROVED")
     return redirect('groups')
 
 @login_required
@@ -37,4 +42,27 @@ def group_remove_user(request, group_id, user_id):
     user.groups.remove(group_request.request_group)
     user.save()
     group_request.delete()
+    notify_user(group_request, "REMOVED")
     return redirect('groups')
+
+# HELPERS
+def notify_discord_channel(group_request, guild):
+    if guild and guild.slug in discord_settings.GUILD_ADMIN_CHANNELS:
+        channel = discord_settings.GUILD_ADMIN_CHANNELS[guild.slug]
+        send_discord_message(
+        channel,
+        "%s (%s) has applied to %s. https://auth.kryptedgaming.com/groups/" % (group_request.request_user.discord, group_request.request_user, group_request.request_group)
+        )
+
+def notify_user(group_request, type):
+    try:
+        channel = "#bot"
+        send_discord_message(
+        channel,
+        "Your group request to %s has been %s." % (group_request.request_group, type),
+        user=group_request.request_user.id
+        )
+    except KeyError:
+        logger.warning("Please define a #bot channel to notify users of group updates.")
+    except Exception as e:
+        logger.warning(e)
