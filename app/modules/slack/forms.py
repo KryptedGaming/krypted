529bc0cad027ebc48a87b131d119808e7445a462
porowns <porowns@gmail.com>
2018-11-11 04:39:59 +0100

    Remove slack modules

--- app/modules/slack/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-from django.contrib.auth.models import Group
-
-class SlackChannelForm(forms.Form):
-    name = forms.CharField(max_length=16)
-    groups = forms.ModelMultipleChoiceField(queryset=Group.objects.all())
--- app/modules/slack/urls.py
+++
@@ -1,8 +0,0 @@
-from modules.slack import views
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^resend/$', views.resend, name='slack_resend'),
-    url(r'^add-slack-channel/$', views.add_slack_channel, name='slack_add_channel'),
-]
