4c01df8e4206926544df8a69b641fdc30f609c4a
porowns <porowns@gmail.com>
2019-01-27 23:36:39 +0100

    Remove records

--- app/modules/records/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from modules.records.models import EventLog, GuildLog, SurveyLog
-
-admin.site.register(EventLog)
-admin.site.register(GuildLog)
-admin.site.register(SurveyLog)
