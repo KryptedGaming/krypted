1105841514cc2f47ea8d6fa6261fe16db21d9a24
porowns <porowns@gmail.com>
2019-04-08 18:20:18 -0400

    Remove EVE group integration from core module

--- app/core/tasks.py
+++ app/core/tasks.py
@@ -20,14 +20,6 @@ def clear_inactive_users():
         if not user.info.discord or not user.info.discourse:
             clear_inactive_user.apply_async(args=[user.id])
 
-@task()
-def audit_eve_groups():
-    logger.debug("Starting EVE group audit")
-    eve_online_primary = GroupIntegration.objects.filter(type="EVE_ONLINE_PRIMARY").exists()
-    eve_online_blue = GroupIntegration.objects.filter(type="EVE_ONLINE_BLUE").exists()
-    for user in User.objects.all():
-        audit_eve_groups_for_user.apply_async(args=[user.pk, eve_online_primary, eve_online_blue])
-
 """
 MINOR TASKS
 These tasks are used by the major tasks. 
@@ -37,46 +29,3 @@ def clear_inactive_user(user_id):
     user = User.objects.get(id=user_id)
     for group in user.groups.all():
         user.groups.remove(group)
-    for guild in user.guilds_in.all():
-        guild.users.remove(user)
-
-@task()
-def audit_eve_groups_for_user(user_id, eve_online_primary_exists, eve_online_blue_exists):
-    if not eve_online_primary_exists and not eve_online_blue_exists:
-        logger.warning("Skipping EVE group audit for userid %s: MISSING_GROUP_INTEGRATIONS" % (user_id))
-        return 
-    if eve_online_primary_exists:
-        primary_group = GroupIntegration.objects.get(type="EVE_ONLINE_PRIMARY").group 
-    else:
-        primary_group = None
-    if eve_online_blue_exists:
-        blue_group = GroupIntegration.objects.get(type="EVE_ONLINE_BLUE").group
-    else:
-        blue_group = None 
-
-    is_primary = False 
-    is_blue = False 
-
-    user = User.objects.get(id=user_id)
-    eve_character = user.info.eve_character
-    if eve_character:
-        if eve_character.is_member():
-            is_primary = True 
-        if eve_character.is_blue():
-            is_blue = True 
-    
-    if is_primary:
-        if primary_group and primary_group not in user.groups.all():
-            user.groups.add(primary_group)
-        if blue_group and blue_group in user.groups.all():
-            user.groups.remove(blue_group)
-    elif is_blue:
-        if blue_group and blue_group not in user.groups.all():
-            user.groups.add(blue_group)
-        if primary_group and primary_group in user.groups.all():
-            user.groups.remove(primary_group)
-    else:
-        if primary_group and primary_group in user.groups.all():
-            user.groups.remove(primary_group)
-        if blue_group and blue_group in user.groups.all():
-            user.groups.remove(blue_group)
