d621bc708e325a8282285cfc0738d256927ce634
porowns <porowns@gmail.com>
2019-08-11 19:07:54 -0400

    Remove Docker files, update Django

--- install/configuration/applications.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig, apps
-from django.conf import settings
-import logging
-
-logger = logging.getLogger(__name__)
-
-
-class ApplicationsConfig(AppConfig):
-    name = 'modules.applications'
