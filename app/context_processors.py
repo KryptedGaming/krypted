013d2b840150e0fa04a6c1c78316783f9c768d94
porowns <porowns@gmail.com>
2019-10-23 14:15:38 -0400

    Add sidebar extension functionality

--- app/context_processors.py
+++ app/context_processors.py
@@ -1,7 +1,21 @@
 from django.conf import settings
-
+import django
 
 def installed_apps(request):
     return {
         'INSTALLED_APPS': settings.INSTALLED_APPS
+    }
+
+def sidebar_extensions(request):
+    templates = []
+    for KRYPTED_APP in settings.KRYPTED_APPS:
+        template_name = "%s/sidebar.html" % KRYPTED_APP.lower()
+        try: 
+            django.template.loader.get_template(template_name) 
+            templates.append(template_name)
+        except Exception as e:
+            pass 
+
+    return {
+        'SIDEBAR_EXTENSIONS': templates
     }
