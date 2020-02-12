e0a1d9c5dec71d3d8a4de06d2a0677546bf3ab40
porowns <porowns@gmail.com>
2020-02-12 12:46:58 -0500

    Change REQUIREMENTS/EXTENSIONS behavior

--- docker/celery/entrypoint.sh
+++ docker/celery/entrypoint.sh
@@ -2,32 +2,25 @@
 export manage_py=/opt/krypted/app/manage.py
 
 echo "Installing Python requirements supplied by environment"
-IFS=',' read -ra REQUIREMENTS <<< "$REQUIREMENTS"
-for REQUIREMENT in ${REQUIREMENTS[@]}; do
-    IFS='/' read -ra REQUIREMENT <<< "$REQUIREMENT"
-    if [ -z "${REQUIREMENT[1]}" ]; then 
-        eval "pip3 install ${REQUIREMENT[0]} > /dev/null"
-    else
-        eval "git clone https://github.com/${REQUIREMENT[0]}/${REQUIREMENT[1]} /opt/${REQUIREMENT[1]} > /dev/null"
-        eval "pip3 install -e /opt/${REQUIREMENT[1]} > /dev/null"
-    fi 
+IFS=',' read -ra PIP_INSTALLS <<< "$PIP_INSTALLS"
+for PIP_INSTALL in ${PIP_INSTALLS[@]}; do
+    eval "pip3 install ${PIP_INSTALL[0]} > /dev/null"
+done 
+IFS=',' read -ra GIT_INSTALLS <<< "$GIT_INSTALLS"
+for GIT_INSTALL in ${GIT_INSTALLS[@]}; do
+    IFS='/' read -ra GIT_INSTALL <<< "$GIT_INSTALL"
+    eval "git clone https://github.com/${GIT_INSTALL[0]}/${GIT_INSTALL[1]} /opt/${GIT_INSTALL[1]} > /dev/null"
+    eval "pip3 install -e /opt/${GIT_INSTALL[1]} > /dev/null"
 done 
 echo "Successfully installed Python requirements"
 
-FORMATTED_EXTENSIONS=""
-IFS=',' read -ra EXTENSIONS <<< "$EXTENSIONS"
-for EXTENSION in ${EXTENSIONS[@]}; do
-    FORMATTED_EXTENSIONS="$FORMATTED_EXTENSIONS '$(echo $EXTENSION | sed s/,//g)',"
-done 
-
+tail -f /dev/null/
 function replace_setting() {
     sed -i -E "s/$1/$2/g" /opt/krypted/app/app/settings.py
 }
 
 # set mysql settings 
 echo "Replacing settings"
-replace_setting "DJANGO_SECRET\s*=\s*'.*'" "DJANGO_SECRET= '${DJANGO_SECRET}'"
-replace_setting "EXTENSIONS\s*=\s*\[.*\]" "EXTENSIONS = [${FORMATTED_EXTENSIONS}]"
 echo "app.conf.broker_url = 'amqp://rabbitmq:5672'" >> /opt/krypted/app/app/celery.py
 
 # run conditional scripts 
