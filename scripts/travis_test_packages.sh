045fda5ae9767a06d2c6ecd8efaf5881a55a5069
porowns <porowns@gmail.com>
2020-02-24 17:43:32 -0500

    Add migrate

--- scripts/travis_test_packages.sh
+++ scripts/travis_test_packages.sh
@@ -17,18 +17,18 @@ done
 echo $INSTALLED_APPS
 pip freeze 
 
+python3 ./app/manage.py migrate 
+
 # test packages 
-python3 ./app/manage.py test django_eveonline_group_states
-python3 ./app/manage.py test django_eveonline_group_states --noinput --settings=app.settings
-# for package in $packages; do 
-#     app=$(basename "$(dirname $package)")
-#     echo "running tests for '$app'"
-#     if [ ! -z "$app" ]; then 
-#         python3 ./app/manage.py test $app --noinput --settings=app.settings
-#         if [ $? -ne 0 ]; then 
-#             exit 1
-#         fi
-#     fi 
-# done 
+for package in $packages; do 
+    app=$(basename "$(dirname $package)")
+    echo "running tests for '$app'"
+    if [ ! -z "$app" ]; then 
+        python3 ./app/manage.py test $app --noinput --settings=app.settings
+        if [ $? -ne 0 ]; then 
+            exit 1
+        fi
+    fi 
+done 
 
 exit 0 
