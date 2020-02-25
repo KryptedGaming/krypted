3f233df899cc7fd4de9ac421319d817e73d6937f
porowns <porowns@gmail.com>
2020-02-25 13:30:10 -0500

    Fix code quality errors

--- scripts/travis_deploy_latest.sh
+++ scripts/travis_deploy_latest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t kryptedgaming/krypted:latest --build-arg VERSION=$TRAVIS_BRANCH ./docker/app/ --no-cache
+docker build -t kryptedgaming/krypted:latest --build-arg VERSION="$TRAVIS_BRANCH" ./docker/app/ --no-cache
 if [ $? -ne 0 ]; then 
     exit 1
 fi 
-docker build -t kryptedgaming/krypted_celery:latest --build-arg VERSION=$TRAVIS_BRANCH ./docker/celery/ --no-cache
+docker build -t kryptedgaming/krypted_celery:latest --build-arg VERSION="$TRAVIS_BRANCH" ./docker/celery/ --no-cache
 if [ $? -ne 0 ]; then 
     exit 1
 fi 
--- scripts/travis_test_packages.sh
+++ scripts/travis_test_packages.sh
@@ -2,29 +2,26 @@
 export INSTALLED_APPS=accounts,group_requests,applications
 pip install --upgrade pip
 # install packages 
-packages=`ls -l app/packages/ | awk '{ print $9 }'`
+packages=$(ls -l app/packages/ | awk '{ print $9 }')
 for package in $packages; do 
-    pip3 install -e ./app/packages/$package
+    pip3 install -e ./app/packages/"$package"
 done 
 
 # add to installed_apps
-packages=`find ./app/packages -name apps.py `
+packages=$(find ./app/packages -name apps.py)
 for package in $packages; do
     app=$(basename "$(dirname $package)")
-    echo $app
     export INSTALLED_APPS=$app,$INSTALLED_APPS
 done 
-echo $INSTALLED_APPS
-pip freeze 
 
 python3 ./app/manage.py migrate 
 
 # test packages 
 for package in $packages; do 
-    app=$(basename "$(dirname $package)")
+    app=$(basename "$(dirname "$package")")
     echo "running tests for '$app'"
     if [ ! -z "$app" ]; then 
-        python3 ./app/manage.py test $app --noinput --settings=app.settings
+        python3 ./app/manage.py test "$app" --noinput --settings=app.settings
         if [ $? -ne 0 ]; then 
             exit 1
         fi
