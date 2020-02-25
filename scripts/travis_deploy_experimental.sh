59882ad7548d951ef11f3aa0ec2f13fcd1b1138d
porowns <porowns@gmail.com>
2020-02-25 10:40:52 -0500

    uncomment docker

--- scripts/travis_deploy_experimental.sh
+++ scripts/travis_deploy_experimental.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-echo "docker build -t kryptedgaming/krypted:experimental --build-arg VERSION=$(git rev-parse --abbrev-ref HEAD) ./docker/app/ --no-cache"
-echo "docker build -t kryptedgaming/krypted_celery:experimental --build-arg VERSION=$(git rev-parse --abbrev-ref HEAD) ./docker/celery/ --no-cache"
-echo "docker push kryptedgaming/krypted:experimental"
-echo "docker push kryptedgaming/krypted_celery:experimental"
+docker build -t kryptedgaming/krypted:experimental --build-arg VERSION=$(git rev-parse --abbrev-ref HEAD) ./docker/app/ --no-cache
+docker build -t kryptedgaming/krypted_celery:experimental --build-arg VERSION=$(git rev-parse --abbrev-ref HEAD) ./docker/celery/ --no-cache
+docker push kryptedgaming/krypted:experimental
+docker push kryptedgaming/krypted_celery:experimental
 
 exit 0 
