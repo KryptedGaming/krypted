01f9bc641ab8d4348ae832a1ea1e309d2cc98abb
porowns <porowns@gmail.com>
2020-02-25 10:03:08 -0500

    add docker creds

--- scripts/travis_deploy_experimental.sh
+++ scripts/travis_deploy_experimental.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "docker build -t kryptedgaming/krypted:latest --build-arg VERSION=$(git rev-parse --abbrev-ref HEAD) ./docker/app/ --no-cache"
-echo "docker build -t kryptedgaming/krypted_celery:latest --build-arg VERSION=$(git rev-parse --abbrev-ref HEAD) ./docker/celery/ --no-cache"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "docker build -t kryptedgaming/krypted:experimental --build-arg VERSION=$(git rev-parse --abbrev-ref HEAD) ./docker/app/ --no-cache"
+echo "docker build -t kryptedgaming/krypted_celery:experimental --build-arg VERSION=$(git rev-parse --abbrev-ref HEAD) ./docker/celery/ --no-cache"
 echo "docker push kryptedgaming/krypted:experimental"
 echo "docker push kryptedgaming/krypted_celery:experimental"
 
