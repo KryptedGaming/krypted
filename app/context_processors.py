29e4b5f23985a9e7022e66cf6af2d8b66a5bddcd
porowns <porowns@gmail.com>
2020-11-23 15:28:24 -0500

    Add SITE LOGO customization

--- app/context_processors.py
+++ app/context_processors.py
@@ -6,6 +6,11 @@ def installed_apps(request):
         'INSTALLED_APPS': settings.INSTALLED_APPS
     }
 
+def site_logo(request):
+    return {
+        'SITE_LOGO': settings.SITE_LOGO
+    }
+
 def google_analytics(request):
     return {
         'GOOGLE_ANALYTICS': settings.GOOGLE_ANALYTICS
