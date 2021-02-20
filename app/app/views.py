062b17e5a08b7fa04992283d3dc9a8413f310c62
porowns <porowns@gmail.com>
2021-02-20 15:36:13 -0500

    Remove individual mark as read until notification rework. Closes #381

--- app/app/views.py
+++ app/app/views.py
@@ -12,24 +12,28 @@ from django.conf import settings
 from notifications.models import Notification
 import json
 
+
 @login_required
 def dashboard(request):
     if 'django_eveonline_connector' in settings.INSTALLED_APPS:
         from django_eveonline_connector.models import PrimaryEveCharacterAssociation
         if request.user.eve_tokens.all().count() > 1 and not PrimaryEveCharacterAssociation.objects.filter(user=request.user).exists():
-            messages.warning(request, "You need to select a primary EVE Online character.")
+            messages.warning(
+                request, "You need to select a primary EVE Online character.")
             return redirect('django-eveonline-connector-character-select-primary')
     return redirect('accounts-user', username=request.user.username)
-    
+
+
 def handler500(request):
     return render(request, '500.html', status=505)
 
+
 @login_required
 def unread_notifications(request):
-    import logging 
+    import logging
     logger = logging.getLogger(__name__)
     from django.http import JsonResponse
-    return JsonResponse({ "data": [
+    return JsonResponse({"data": [
         {
             "timestamp": f"{notification.timesince()} ago",
             "verb": notification.verb,
@@ -67,10 +71,11 @@ def unread_notifications(request):
         in request.user.notifications.unread().exclude(public=False)
     ]}, safe=False)
 
+
 @login_required
 def unread_system_notifications(request):
     from django.http import JsonResponse
-    return JsonResponse({ "data": [
+    return JsonResponse({"data": [
         {
             "timestamp": f"{notification.timesince()} ago",
             "verb": notification.verb,
@@ -80,7 +85,6 @@ def unread_system_notifications(request):
                 <button type="button" class="btn btn-info" data-toggle="modal" data-target="#modal-%s">
                   View Details
                 </button>
-                <button class="btn btn-success">Mark as Read</button>
             </div>
             <div class="modal fade" id="modal-%s" style="padding-right: 17px;" aria-modal="true">
                 <div class="modal-dialog">
@@ -116,6 +120,7 @@ def unread_system_notifications(request):
         in request.user.notifications.unread().exclude(public=True)
     ]}, safe=False)
 
+
 @login_required
 def mark_as_read(request, notification_pk):
     if not Notification.objects.filter(pk=pk).exists():
@@ -123,6 +128,6 @@ def mark_as_read(request, notification_pk):
     notification = Notification.objects.get(pk=notification_pk)
     if request.user != notification.recipient:
         return HttpResponse(status=403)
-    
+
     notification.mark_as_read()
     return HttpResponse(status=200)
